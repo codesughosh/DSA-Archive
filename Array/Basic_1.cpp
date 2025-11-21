13fb387a2590243441f3bb73137f4d81dcd37224
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2025-11-21 21:18:17 +0530

    Largest Element in an Array

---
+++ Array/Basic_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int findLargestElement(int arr[], int n) {
+ 
+  int max = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
